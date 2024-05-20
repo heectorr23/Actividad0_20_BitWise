a6e8ca39ca3a7fb53d279bde9aeecda2eec84293
Hector Olivera <hector.olivera@campusfp.es>
2024-05-20 18:10:37 +0200

    Comienzo del Proyecto

---
+++ src/main/java/com/empresa/actividad0_20_bitwise/HelloController.java
@@ -0,0 +1,14 @@
+package com.empresa.actividad0_20_bitwise;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.empresa.actividad0_20_bitwise {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.empresa.actividad0_20_bitwise to javafx.fxml;
+    exports com.empresa.actividad0_20_bitwise;
+}
